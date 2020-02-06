87eeffd4db7321aecef2012a171cc45e0d78d751
Oliver Geneser <32340667+Oliv6176@users.noreply.github.com>
2020-02-06 11:14:54 +0100

    API

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from kivy.app import App
-kivy.uix.widget import Widget
+from kivy.uix.widget import Widget
+import requests
 
-class DnD(Widget):
-    pass
+class DnD():
+    res = requests.get("http://www.dnd5eapi.co/api/")
+    for i in res.json():
+        print(i)
 
 class DnDApp(App):
     def build(self):
         return DnD()
 
 if __name__ == "__main__":
-    DnDApp().run()
+    #DnDApp().run()
+    DnD()
